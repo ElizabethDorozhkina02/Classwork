489c2673c76e4735776c374419d2157f86111c13
Elizabeth <elizavetadorozhkina02@gmail.com>
2025-04-29 20:03:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# a = int(input("Введите год своего рождения: "))
+# if 1925<a<1944:
+#     print("Молчаливое поколение")
+# elif 1944<a<1967:
+#     print("Поколение бэби-бумеров")
+# elif 1967<a<1984:
+#     print("Поколение X")
+# elif 1984 < a < 2000:
+#     print("Поколение Y")
+# elif 2000 < a < 2011:
+#     print("Поколение Z")
+# elif a > 2011:
+#     print("Поколение альфа")
+
+
+# a = int(input("num1: "))
+# b = int(input("num2: "))
+# for i in range(a, b + 1):
+#     if i%3==0:
+#         print(i)
+# avg = ()
+# print("Среднее арифметическое чисел кратных 3" , avg)
+
+# a = int(input("Введите диапазон от: "))
+# b = int(input("Введите диапазон до: "))
+# n = int(input("Введите число: "))
+# if a>b: a,b=b,a
+# elif 
+
+a = int(input("Введите сторону квадрата: "))
+print("+" * a)
+for i in range(a):
+        print("+" * a)
